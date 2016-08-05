66984ee9dc14140f2acd0afa594b722937d99c6f
2smallears <2smallears@users.noreply.github.com>
2016-08-05 09:54:54 +0800

    delete formItem

--- js/app.js
+++ js/app.js
@@ -13,12 +13,17 @@ const App = React.createClass({
     items.push(item);
     this.setState({items});
   },
+  delete: function (index) {
+    const items = this.state.items;
+    items.splice(index, 1);
+    this.setState({items});
+  },
   render: function() {
     const isEdit = this.state.isEdit;
     return <div>
       <button onClick={this.toggle}>{isEdit ? "Preview" : "Edit"}</button>
       <div className={isEdit ? "" : "hidden"}>
-        <Edit onAdd={this.add} items={this.state.items}/>
+        <Edit onAdd={this.add} items={this.state.items} onDelete={this.delete}/>
       </div>
       <div className={isEdit ? "hidden" : ""}>
         <Preview />
@@ -29,16 +34,20 @@ const App = React.createClass({
 const Edit = React.createClass({
   render: function() {
     return <div>
-      <Left items={this.props.items}/>
+      <Left items={this.props.items} onDelete={this.props.onDelete}/>
       <Right onAdd={this.props.onAdd}/>
     </div>;
   }
 });
 const Left = React.createClass({
+  delete: function (index) {
+    this.props.onDelete(index);
+  },
   render: function() {
     const items = this.props.items.map((item, index) => {
       return <div key={index}>
         <input type={item} />
+        <button onClick={this.delete.bind(this, index)}>X</button>
       </div>
     })
     return <div>
