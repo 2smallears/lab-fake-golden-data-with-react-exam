f54f3c49eca3ea19364df453f69ad3a022cdd707
2smallears <2smallears@users.noreply.github.com>
2016-08-05 09:46:13 +0800

    toggle preview and edit

--- js/app.js
+++ js/app.js
@@ -1,87 +1,37 @@
 const App = React.createClass({
   getInitialState: function () {
     return {
-      isEdit: true,
-      items: []
+      isEdit: true
     }
   },
   toggle: function () {
     this.setState({isEdit: !this.state.isEdit});
-  },
-  add: function (item) {
-    const items = this.state.items;
-    items.push(item);
-    this.setState({items});
-  },
-  delete: function (index) {
-    const items = this.state.items;
-    items.splice(index, 1);
-    this.setState({items});
   },
   render: function() {
     const isEdit = this.state.isEdit;
     return <div>
       <button onClick={this.toggle}>{isEdit ? "Preview" : "Edit"}</button>
       <div className={isEdit ? "" : "hidden"}>
-        <Edit onAdd={this.add} items={this.state.items} onDelete={this.delete}/>
+        <Edit />
       </div>
       <div className={isEdit ? "hidden" : ""}>
-        <Preview items={this.state.items}/>
+        <Preview />
       </div>
     </div>;
   }
 });
-
 const Edit = React.createClass({
   render: function() {
     return <div>
-      <Left items={this.props.items} onDelete={this.props.onDelete}/>
-      <Right onAdd={this.props.onAdd}/>
-    </div>;
-  }
-});
-
-const Left = React.createClass({
-  delete: function (index) {
-    this.props.onDelete(index);
-  },
-  render: function() {
-    const items = this.props.items.map((item ,index) => {
-      return <div key={index}>
-        <input type={item} />
-        <button onClick={this.delete.bind(this, index)}>X</button>
-      </div>
-    })
-    return <div>
-      {items}
-    </div>;
-  }
-});
-
-const Right = React.createClass({
-  add: function () {
-    const item = $("input[name=item]:checked").val();
-    this.props.onAdd(item);
-  },
-  render: function() {
-    return <div>
-      <input type="radio" name="item" value="text" />文本
-      <input type="radio" name="item" value="date" />日期
-      <button onClick={this.add}>+</button>
+      Edit
     </div>;
   }
 });
 const Preview = React.createClass({
   render: function() {
-    const items = this.props.items.map((item, index) => {
-      return <div key={index}>
-        <input type={item} />
-      </div>
-    });
     return <div>
-      {items}
+      Preview
     </div>;
   }
 });
-
-ReactDOM.render(<App />,document.getElementById('content'));
+ReactDOM.render(<App />, document.getElementById('content'));
